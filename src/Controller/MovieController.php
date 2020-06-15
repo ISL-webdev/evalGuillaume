75a59970120463cb9e204c90999aafa640194d68
Guillaume Malecha <guillaume.malecha@gmail.com>
2020-06-15 21:55:00 +0200

    update

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use http\Env\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,4 +18,75 @@ class MovieController extends AbstractController
             'controller_name' => 'MovieController',
         ]);
     }
+
+    /**
+     * @Route("/addmovie", name="addmovie")
+     */
+    public function addmovie()
+    {
+        $movie = new Movie();
+        $movie->setTitle('')
+            ->setGenre('')
+            ->setActor('');
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($movie);
+        $entityManager->flush();
+        return $this->render('movie/addmovie.html.twig', [
+            'controller_name' => 'MovieController',
+        ]);
+    }
+
+    /**
+     * @Route("/genre", name="genre")
+     */
+    public function genre()
+    {
+        return $this->render('movie/genre.html.twig', [
+            'controller_name' => 'MovieController',
+        ]);
+    }
+
+
+
+    /**
+     * @Route("/movie/{id}", name="show_movie")
+     */
+    public function show($id)
+    {
+        $movie = $this->getDoctrine()
+            ->getRepository(movie::class)
+            ->find($id);
+        return $this->render('movie/showmovie.html.twig', [
+            'movie' => '$movie',
+        ]);
+    }
+
+    /**
+     * @Route("/allmovies", name="allmovies")
+     */
+    public function allmovies()
+    {
+        $movie = $this->getDoctrine()
+            ->getRepository(movie::class)
+            ->find($id);
+        return $this->render('movie/allmovies.html.twig', [
+            'controller_name' => 'MovieController',
+        ]);
+    }
+
+
+    /**
+ * @Route("/addmovie", name="movies")
+ *
+public function addmovie(): Response
+    {
+        $movie = new Movie();
+        $movie->setTitle('Interstellar')
+            ->setGenre('SciFi')
+            ->setActor('Matthew McConaughey');
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($movie);
+        $entityManager->flush();
+        return new Response("Nouveau film ajouté : ".$movie->getTitle());
+    }*/
 }
--- src/Entity/Movie.php
+++ src/Entity/Movie.php
@@ -17,8 +17,59 @@ class Movie
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Genre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Actor;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->Title;
+    }
+
+    public function setTitle(string $Title): self
+    {
+        $this->Title = $Title;
+
+        return $this;
+    }
+
+    public function getGenre(): ?string
+    {
+        return $this->Genre;
+    }
+
+    public function setGenre(string $Genre): self
+    {
+        $this->Genre = $Genre;
+
+        return $this;
+    }
+
+    public function getActor(): ?string
+    {
+        return $this->Actor;
+    }
+
+    public function setActor(string $Actor): self
+    {
+        $this->Actor = $Actor;
+
+        return $this;
     }
 }
